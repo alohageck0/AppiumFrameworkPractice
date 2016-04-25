0f9b715fd7af2e2d6ec78af350c9f6ccf510d5b0
alohageck0 <royalfiish@gmail.com>
2016-04-24 22:24:41 -0700

    04/24/16 home practice

--- src/test/java/TestNGFiles/Annotations.java
+++ src/test/java/TestNGFiles/Annotations.java
@@ -1,12 +1,36 @@
 package TestNGFiles;
 
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 public class Annotations {
+   @BeforeTest
+   public void CleanCookies(){
+      System.out.println("Clean cookies before all tests");
+   }
+   @AfterTest
+   public void DeleteCookies(){
+      System.out.println("Deleting cookies after comlpiting all tests");
+   }
+
+   @BeforeMethod
+   public void UserIDGeneration() {
+      System.out.println("generate ID");
+   }
+
 
    @Test
    //Opens browser
-   public void Opening() {
+   public void AOpening() {
       System.out.println("Open browser");
+   }
+
+   @Test
+   public void FlightBooking() {
+      System.out.println("Flight booking");
+
+
    }
 }
