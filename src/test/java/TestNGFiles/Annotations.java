7a8a13a1dd399fbc53f939e44fde3b7439093339
alohageck0 <royalfiish@gmail.com>
2016-04-24 22:09:15 -0700

    04/24/16 home practice

---
+++ src/test/java/TestNGFiles/Annotations.java
@@ -0,0 +1,12 @@
+package TestNGFiles;
+
+import org.testng.annotations.Test;
+
+public class Annotations {
+
+   @Test
+   //Opens browser
+   public void Opening() {
+      System.out.println("Open browser");
+   }
+}
